c34154c353f61fba2663c504ddc53f0050a0941d
VSoups <vince.kubota@gmail.com>
2024-06-06 10:57:13 -0700

    Detail url working

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('packages/', views.packages_index, name='index'),
-
+    path('packages/<int:pkg_id>', views.package_detail, name='package_detail'),
 ]
