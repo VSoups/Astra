36b5da7d5edba57b69904c314565d29e3e49ce00
VSoups <vince.kubota@gmail.com>
2024-06-11 14:30:08 -0700

    Likes Working

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,16 +6,15 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('packages/', views.packages_index, name='index'),
+    path('packages/<int:pk>/reviews/', views.ReviewsForPackage.as_view(), name='reviews_index'),
     path('packages/<int:pkg_id>/<str:picked_date>/', views.package_detail, name='package_detail'),
     path('tickets/<int:pkg_id>/add_ticket/', views.add_ticket, name='add_ticket'),
     path('tickets/history/', views.ticket_index, name='ticket_index'),
     path('reviews/<int:pkg_id>/create/', views.ReviewCreate.as_view(), name='reviews_create'),
-
-    path('reviews/', views.ReviewList.as_view(), name='reviews_index'),
-    # path('reviews/<pkg_id>/', views.ReviewList.as_view(), name='reviews_index'),
-
     path('reviews/<int:pk>/', views.ReviewDetail.as_view(), name='reviews_detail'),
     path('reviews/<int:pk>/update/', views.ReviewUpdate.as_view(), name='reviews_update'),
     path('reviews/<int:pk>/delete/', views.ReviewDelete.as_view(), name='reviews_delete'),
+    path('packages/<int:pkg_id>/reviews/<int:review_id>/liked', views.like_review, name='like_review'),
+    path('packages/<int:pkg_id>/reviews/<int:review_id>/unliked', views.unlike_review, name='unlike_review'),
 ]
 
